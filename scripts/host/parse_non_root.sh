d5e609b87b164d89737fb7833c0f4472f4824ccb
Rostislav Frolov <panzerslava2802@mail.ru>
2025-03-26 00:00:10 +0000

    Add `vcpkg` support and rework `Dockerfile`
    - Utilise parallel building in `Dockerfile`. Extensively.
    - Add `vcpkg` to container
    - Switch to building `ninja` from source instead of installing the
      package
    - Rename buildstage `dev` to `cmake`
    - Add following targets to `Dockerfile`:
      - Services:
        - `downloads_srv` - `alpine/git:latest` - clones repositories to
          /srv/downloads
        - `ninja_srv` - `cmake` - builds and installs (system-wide) `ninja`,
          then copies `ninja` binary to /srv/ninja
        - `vcpkg_srv` - `cmake` - installs and bootstraps `vcpkg`
          (system-wide). Tries to build as much dependencies from manifest as
          possible (`--keep-going`) to populate download and build cache.
          Produced caches, installation and installed dependencies are then
          symlinked in /srv/vcpkg
      - Intermediate stages:
        - `ninja` - `cmake` - installs (system-wide) `ninja` (from
          `ninja_srv`) and symlinks it (`ninja-build`)

--- scripts/host/parse_non_root.sh
+++ scripts/host/parse_non_root.sh
@@ -2,7 +2,6 @@
 
 # change pwd to directory, where this script is stored
 : ${curdir:="$(dirname -- $(readlink -f -- "$0"))"}
-cd $curdir
 
 # parse IDs to non-root target of Dockerfile
 sed --in-place \
@@ -10,4 +9,4 @@ sed --in-place \
     -e"s/^ARG USER_NAME.*/ARG USER_NAME=$(id -un)/g" \
     -e"s/^ARG USER_GID.*/ARG USER_GID=$(id -g)/g" \
     -e"s/^ARG USER_GROUP.*/ARG USER_GROUP=$(id -gn)/g" \
-    Dockerfile
+    $curdir/../../.devcontainer/Dockerfile
